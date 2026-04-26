807d650aed1711d94205a9fb003ea48e64c920fd
garçoni-leonardo <leonardorochagarconi@gmail.com>
2026-04-26 02:48:45 -0300

    Corrigindo problema de incompatibilidade entre o Lombok e o Java 21

---
+++ ecomel-api/src/main/java/br/com/ecomel/EcomelApplication.java
@@ -0,0 +1,11 @@
+package br.com.ecomel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcomelApplication.class, args);
+    }
+}
